18fba871b2e81650818c89a862862bf3615f0412
RebecaTI <mebuffbeka@gmail.com>
2025-05-07 10:54:34 -0300

    refactor: remove unused functions and streamline type handling

--- TypeScript/05-types-interfaces-1/script.js
+++ TypeScript/05-types-interfaces-1/script.js
@@ -1,20 +1,13 @@
 "use strict";
-function preencherDados(dados) {
-    document.body.innerHTML += `
-    <div>
-      <h2>${dados.nome}</h2>
-      <p>R$ ${dados.preco}</p>
-      <p>Inclui teclado: ${dados.preco ? 'sim' : 'nao'}</p>
-    </div>
-  `;
+let total = 200;
+total = '4000';
+function isNumber(value) {
+    if (typeof value === 'number') {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-preencherDados({
-    nome: 'Computador',
-    preco: 2000,
-    teclado: true
-});
-preencherDados({
-    nome: 'Notebook',
-    preco: 2500,
-    teclado: false
-});
+const button = document.querySelector('button');
+button?.click();
--- TypeScript/05-types-interfaces-1/script.ts
+++ TypeScript/05-types-interfaces-1/script.ts
@@ -1,46 +1,13 @@
-type NumberOrString = string | number
+let total : string | number = 200;
+total = '4000';
 
-let total: NumberOrString = 20
-total = '30'
-
-type Produto = {
-  nome: string
-  preco: number
-  teclado: boolean
+function isNumber(value: string | number) {
+  if(typeof value === 'number') {
+    return true
+  } else {
+    return false
+  }
 }
 
-function preencherDados(dados: Produto) {
-  document.body.innerHTML += `
-    <div>
-      <h2>${dados.nome}</h2>
-      <p>R$ ${dados.preco}</p>
-      <p>Inclui teclado: ${dados.preco ? 'sim' : 'nao'}</p>
-    </div>
-  `
-}
-
-const computador: Produto = {
-  nome: 'Computador',
-  preco: 2000,
-  teclado: true
-}
-
-preencherDados({
-  nome: 'Computador',
-  preco: 2000,
-  teclado: true
-})
-
-preencherDados({
-  nome: 'Notebook',
-  preco: 2500,
-  teclado: false
-})
-
-type Categorias = 'design' | 'codigo' | 'descod'
-
-function pintarCategoria(Categoria: string) {
-  console.log(Categoria)
-}
-
-pintarCategoria('teste')
+const button = document.querySelector('button')
+button?.click();
