d030acb7374b95d2fa75f15299e4eede842681cb
RebecaTI <mebuffbeka@gmail.com>
2025-05-15 15:06:44 -0300

    feat: add HTML structure and TypeScript files for product management

--- React/06-205-Ferrament-FrontEnd/src/App.jsx
+++ React/06-205-Ferrament-FrontEnd/src/App.jsx
@@ -1,40 +1,18 @@
 import React from 'react'
-import useLocalStorage from './useLocalStorage'
-import useFetch from './useFetch';
 
 const App = () => {
-  const [produto, setProduto] = useLocalStorage('produto', '');
-  const { request, data, loading, error } = useFetch()
-
-  React.useEffect(() => {
-    async function fetchData() {
-      const { response, json } = await request('https://ranekapi.origamid.dev/json/api/produto/')
-      console.log(response, json)
-    }
-    fetchData()
-  }, [request]);
-
-  function handleClick({ target }) {
-    setProduto(target.innerText)
-  }
-
-  if (error) return <p>{error}</p>
-  if (loading) return <p>Carregando</p>
-  if (data)
-    return (
-      <div>
-        <p>Produto preferido: {produto}</p>
-        <button onClick={handleClick}> notebook </button>
-        <button onClick={handleClick}> smartphone </button>
-
-        {data.map((produto) => (
-          <div key={produto.id}>
-            <h1>{produto.nome}</h1>
-          </div>
-        ))}
-      </div>
-    )
-  else return null;
+  const [nome, setNome] = React.useState('');
+  return (
+    <form>
+      <label hmlFor="nome">Nome</label>
+      <input
+        id="nome"
+        type="text"
+        value={nome}
+        onChange={(event) => setNome(event.target.value)} />
+      {nome}
+    </form>
+  )
 }
 
 export default App
---
+++ TypeScript/Typescript/script.js
@@ -0,0 +1,39 @@
+"use strict";
+class Produto {
+    nome;
+    preco;
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    precoReal() {
+        return 'R$ ';
+    }
+}
+const livro = new Produto('A Guerra dos Tronos', 200);
+console.log(livro.nome);
+class Livro {
+    autor;
+    constructor(autor) {
+        this.autor = autor;
+    }
+}
+class Jogo {
+    jogadores;
+    constructor(jogadores) {
+        this.jogadores = jogadores;
+    }
+}
+function buscarProduto(busca) {
+    if (busca === 'O Hobbit') {
+        return new Livro('J. R. R. Tolkien');
+    }
+    if (busca === 'Dark Souls') {
+        return new Jogo(1);
+    }
+    return null;
+}
+const produto = buscarProduto('O Hobbit');
+if (produto instanceof Livro) {
+    produto.autor;
+}
