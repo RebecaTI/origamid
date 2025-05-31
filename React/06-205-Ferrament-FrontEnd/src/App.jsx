e31db82824273a06d59b0e9731fb19a4a0cc76b8
RebecaTI <mebuffbeka@gmail.com>
2025-05-31 14:16:28 -0300

    Aula sobre textarea

--- React/06-205-Ferrament-FrontEnd/src/App.jsx
+++ React/06-205-Ferrament-FrontEnd/src/App.jsx
@@ -1,41 +1,14 @@
 import React from 'react'
 
 const App = () => {
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: ''
-  })
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(event)
-  }
-
-  function handleChange({ target }) {
-    const { id, value } = target;
-    console.log(id, value)
-    setForm({ ...form, [id]: value });
-  }
+  const [textarea, setTextarea] = React.useState('')
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label hmlFor="nome">Nome</label>
-      <input
-        id="nome"
-        type="text"
-        name="nome"
-        value={form.nome}
-        onChange={handleChange} />
-      {form.nome}
-      <label hmlFor="email">Email</label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        value={form.email}
-        onChange={handleChange} />
-      {form.email}
-      <button>Enviar</button>
+    <form>
+      <textarea value={textarea} onChange={({ target }) => setTextarea(target.value)}
+        rows='5'
+      />
+      {textarea}
     </form>
   )
 }
